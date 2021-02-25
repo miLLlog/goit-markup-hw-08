6b4d022bbc67e3ec82bdefd13d8cc7b0caf2325a
miLLlog <milllog@mail.ru>
2021-02-26 00:09:30 +0300

    cделал header-mobile, пофиксил модал

--- js/modal.js
+++ js/modal.js
@@ -7,6 +7,18 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
+    document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const 
+    menuBtnRef = document.querySelector("[data-menu-button]");
+    mobileMenuRef = document.querySelector("[data-menu]");
+  menuBtnRef.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('is-open');
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+  })();

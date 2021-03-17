dfe02d0aab571e16a6683675642a03c07d4a16a7
miLLlog <milllog@mail.ru>
2021-03-17 20:19:48 +0300

    сделал портфолио и картинки

--- js/modal.js
+++ js/modal.js
@@ -12,13 +12,3 @@
   }
 })();
 
-(() => {
-  const 
-    menuBtnRef = document.querySelector("[data-menu-button]");
-    mobileMenuRef = document.querySelector("[data-menu]");
-  menuBtnRef.addEventListener('click', () => {
-    menuBtnRef.classList.toggle('is-open');
-
-    mobileMenuRef.classList.toggle('is-open');
-  });
-  })();
